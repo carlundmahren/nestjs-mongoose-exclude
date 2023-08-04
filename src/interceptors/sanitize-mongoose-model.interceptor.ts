dc41c01aebdd0566480ab6b5163220c1d7d4a6fc
Cornelius Carl <corneliuscarl@me.com>
2023-08-04 19:50:01 +0200

    fix: only transform data if not a pure express object

--- src/interceptors/sanitize-mongoose-model.interceptor.ts
+++ src/interceptors/sanitize-mongoose-model.interceptor.ts
@@ -1,9 +1,9 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Transformer, defaultTransformOptions } from '../Transformer';
 import { TransformOptions } from '../interfaces/transform-options.interface';
 import { storage } from '../storage';
-import { defaultTransformOptions, Transformer } from '../Transformer';
 
 @Injectable()
 export class SanitizeMongooseModelInterceptor implements NestInterceptor {
@@ -12,9 +12,19 @@ export class SanitizeMongooseModelInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       map((data) => {
-        const transformer = new Transformer(storage, this.transformOptions);
-        const transformedData = transformer.transform(data);
-        return transformedData;
+        if (
+          typeof data === 'object' &&
+          Object.keys(data).includes('req') &&
+          Object.keys(data).includes('socket') &&
+          Object.keys(data).includes('locals') &&
+          Object.keys(data).includes('writeHead')
+        ) {
+          return data;
+        } else {
+          const transformer = new Transformer(storage, this.transformOptions);
+          const transformedData = transformer.transform(data);
+          return transformedData;
+        }
       }),
     );
   }
